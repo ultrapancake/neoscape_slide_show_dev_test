a8d1084d61e48eea18d0e9e08fe259f712934c08
Glen Larsen <ultrapancake@gmail.com>
2022-08-29 01:25:17 -0600

    added swiping functionality; near complete re-write

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # neoscape_slide_show_dev_test
 Front End Dev Test for Neoscape. Build a responsive slideshow from scratch.
+# Design Decisions
+I honestly did not put much thought into the design of this project, the majority of my time was spent on simply getting the functionality completed.
+# Development Decisions
+My first pass at the slide show was to add and remove style display none from the slide div that contains the image.  I also added some animation to the slides fading in and out.  This worked well for navigating through the slides using the arrows or navigation dots.  I realized that part of the dev test was to also add swiping functionality.
+Swiping was very difficult for me to implement, I had to completely re-think my approach to the project.  I ended up using the style transform: translateX(0) to manipulate the slides and give the feeling of dragging the slide.  I also added a break point to snap to the next slide and re-used that snapping function to also navigate by the buttons and dots. 
+# Future Improvement
+I would like to improve the Design of this project and add some of the Bonus functionality.
--- script.js
+++ script.js
@@ -1,32 +1,118 @@
-let index = 1;
-showSlides(index);
-// arrow functions
+const slider = document.querySelector('.slider-container'),
+  slides = Array.from(document.querySelectorAll('.slide')),
+  dots = Array.from(document.querySelectorAll('.dot'))
+
+// set up our state
+let isDragging = false,
+  startPos = 0,
+  currentTranslate = 0,
+  prevTranslate = 0,
+  animationID,
+  currentIndex = 0
+
+// add our event listeners
+slides.forEach((slide, index) => {
+  const slideImage = slide.querySelector('img');
+  // disable default image drag
+  slideImage.addEventListener('dragstart', (e) => e.preventDefault());
+  // touch events
+  slide.addEventListener('touchstart', touchStart(index));
+  slide.addEventListener('touchend', touchEnd);
+  slide.addEventListener('touchmove', touchMove);
+  // mouse events
+  slide.addEventListener('mousedown', touchStart(index));
+  slide.addEventListener('mouseup', touchEnd);
+  slide.addEventListener('mousemove', touchMove);
+  slide.addEventListener('mouseleave', touchEnd);
+})
+
+// make responsive to viewport changes
+window.addEventListener('resize', setPositionByIndex);
+
+// prevent menu popup on long press
+window.oncontextmenu = function (event) {
+  event.preventDefault();
+  event.stopPropagation();
+  return false;
+}
+
+showSlides(0);
+
+function getPositionX(event) {
+  return event.type.includes('mouse') ? event.pageX : event.touches[0].clientX;
+}
+
+function touchStart(index) {
+  return function (event) {
+    currentIndex = index;
+    startPos = getPositionX(event);
+    isDragging = true;
+    animationID = requestAnimationFrame(animation);
+    slider.classList.add('grabbing');
+  }
+}
+
+function touchMove(event) {
+  if (isDragging) {
+    const currentPosition = getPositionX(event);
+    currentTranslate = prevTranslate + currentPosition - startPos;
+  }
+}
+
+function touchEnd() {
+  cancelAnimationFrame(animationID);
+  isDragging = false;
+  const movedBy = currentTranslate - prevTranslate;
+
+  // if moved enough negative then snap to next slide if there is one
+  if (movedBy < -100 && currentIndex < slides.length - 1) currentIndex += 1;
+
+  // if moved enough positive then snap to previous slide if there is one
+  if (movedBy > 100 && currentIndex > 0) currentIndex -= 1;
+
+  setPositionByIndex();
+
+  slider.classList.remove('grabbing');
+}
+
+function animation() {
+  setSliderPosition();
+  if (isDragging) requestAnimationFrame(animation);
+}
+
+function setPositionByIndex() {
+  currentTranslate = currentIndex * -window.innerWidth;
+  prevTranslate = currentTranslate;
+  setSliderPosition();
+  setDots();
+}
+
+function setSliderPosition() {
+  slider.style.transform = `translateX(${currentTranslate}px)`;
+}
+
 function moveSlide(e){
-    showSlides(index += e);
+    showSlides(currentIndex += e);
 }
-// dot function
+
 function currentSlide(e){
-    showSlides(index = e);
+    showSlides(currentIndex = e);
 }
-//manipulate slide
+
 function showSlides(e){
-    //get front-end elements
-    let slides = document.getElementsByClassName("slide");
-    let dots = document.getElementsByClassName("dot");
     //if next @ end of slide loop to begining
-    if(e > slides.length){index = 1}
+    if(e > slides.length - 1){currentIndex = 0}
     //if prev @ start loop to end
-    if(e < 1){index = slides.length}
-    //clear slides
-    for (let i = 0; i < slides.length; i++){
-        slides[i].style.display = "none"
-    }
+    if(e < 0){currentIndex = slides.length - 1}
+    setPositionByIndex();
+    setDots();
+}
+
+function setDots(){
     //clear dots
     for(let i = 0; i < dots.length; i++){
         dots[i].classList.remove("active");
     }
-    //set slide
-    slides[index-1].style.display = "block";
     //set dot
-    dots[index-1].classList.add("active");
+    dots[currentIndex].classList.add("active");
 }
